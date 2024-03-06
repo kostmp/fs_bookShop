9f7dc3d727b8e6f4e0216626c99e9193e6811722
Konstantinos Bouzios <kbouzios@deloitte.gr>
2024-03-06 10:50:04 +0000

    View transactions based on a book

--- app/project1/webapp/controller/Transactions.controller.js
+++ app/project1/webapp/controller/Transactions.controller.js
@@ -8,8 +8,19 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("project1.controller.Transactions", {
-            onInit: function () {
-
+            onInit: function () {   
+                let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.getRoute("RouteTransactions").attachPatternMatched(this._onObjectMatched, this);
+            } ,   
+            _onObjectMatched: function(oEvent) {
+                let sBookId = oEvent.getParameter("arguments").ID;
+                console.log(sBookId);
+                this.getView().bindElement({
+                    path: "/Books(" + sBookId + ")", 
+                    parameters: {
+                        expand: "customer" // Assuming you want to display customer details in transactions
+                    }
+                });
             }
-        });
     });
+    })
